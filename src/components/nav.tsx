c9f224cd09ff69dd981eec03d81f13f5baedd3b6
MarleMGN <m0b.torres23@gmail.com>
2026-09-15 20:14:28 -0700

    nav tweaks + added sections

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,26 +1,36 @@
 import React from "react";
-import "../components/nav.css"
+import "../components/nav.css";
 import Image from "next/image";
 
-
-const Nav = () => {
+const Nav = ({ showLogo }: { showLogo: boolean }) => {
   return (
     <>
       <nav>
         <div className="nav__container">
-          <div className="nav__icon-container">
-            <Image src="/HeavenlyLogo.jpg" alt="logo" width={150} height={150} className="nav__icon" />
-          </div>
-          <ul className="nav__links">
+          <ul className="nav__links-left" style={{ marginRight: showLogo ? undefined : "10px" }}>
             <li className="nav__list">
               <div className="nav__link">Home</div>
             </li>
             <li className="nav__list">
               <div className="nav__link">About</div>
             </li>
-            <li className="nav__list">
+          </ul>
+          <div className={`nav__icon-container ${showLogo ? "" : "hidden"}`}>
+              <Image
+                src="/HeavenlyLogo.jpg"
+                alt="logo"
+                width={150}
+                height={150}
+                className="nav__icon"
+              />
+            </div>
+          <ul className="nav__links-right" style={{ marginLeft: showLogo ? undefined : "10px" }}>
+            <div className="nav__list">
               <div className="nav__link">Events</div>
-            </li>
+            </div>
+            <div className="nav__list">
+              <div className="nav__link">Contact</div>
+            </div>
           </ul>
         </div>
       </nav>
