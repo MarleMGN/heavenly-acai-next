527fe53598f8da756c287d231c234bb168776b68
MarleMGN <m0b.torres23@gmail.com>
2026-09-17 15:06:04 -0700

    menu tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ const socialImages = ["/img6.jpg", "/img7.jpg", "/img8.jpg", "/img9.jpg"];
 export default function Home() {
   const [slideIndex, setSlideIndex] = useState(0);
   const [showLogo, setShowLogo] = useState(false);
+  const [openTab, setOpenTab] = useState("");
   const slideshowRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -35,6 +36,11 @@ export default function Home() {
     const interval = setInterval(nextSlide, 5000);
     return () => clearInterval(interval);
   }, []);
+
+  const toggleTab = (tabName: string) => {
+  setOpenTab(openTab === tabName ? "" : tabName);
+};
+
   return (
     <>
       <div className="container">
@@ -98,7 +104,90 @@ export default function Home() {
             <div className="menu__container">
               <div className="menu__content">
                 <div className="menu__title purple">Our Menu</div>
-                <div className="menu"></div>
+                <div className="menu__img-wrapper">
+                  <div className="menu">
+                    <div className={`menu__tab ${openTab === "signature" ? "tab-open" : ""}`} onClick={() => toggleTab("signature")}>
+                      <div className="menu__tab-text purple">
+                        Signature Bowls
+                      </div>
+                      <div className="menu__tab-x">
+                        <svg
+                          stroke="currentColor"
+                          fill="none"
+                          strokeWidth="0"
+                          viewBox="0 0 24 24"
+                          height="1em"
+                          width="1em"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z"
+                            fill="currentColor"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div className={`menu__tab ${openTab === "specialty" ? "tab-open" : ""}`} onClick={() => toggleTab("specialty")}>
+                      <div className="menu__tab-text purple">
+                        Specialty Bowls
+                      </div>
+                      <div className="menu__tab-x">
+                        <svg
+                          stroke="currentColor"
+                          fill="none"
+                          strokeWidth="0"
+                          viewBox="0 0 24 24"
+                          height="1em"
+                          width="1em"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z"
+                            fill="currentColor"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div className={`menu__tab ${openTab === "drinks" ? "tab-open" : ""}`} onClick={() => toggleTab("drinks")}>
+                      <div className="menu__tab-text purple">Drinks</div>
+                      <div className="menu__tab-x">
+                        <svg
+                          stroke="currentColor"
+                          fill="none"
+                          strokeWidth="0"
+                          viewBox="0 0 24 24"
+                          height="1em"
+                          width="1em"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z"
+                            fill="currentColor"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div className={`menu__tab ${openTab === "bites" ? "tab-open" : ""}`} onClick={() => toggleTab("bites")}>
+                      <div className="menu__tab-text purple">Bites</div>
+                      <div className="menu__tab-x">
+                        <svg
+                          stroke="currentColor"
+                          fill="none"
+                          strokeWidth="0"
+                          viewBox="0 0 24 24"
+                          height="1em"
+                          width="1em"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z"
+                            fill="currentColor"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
